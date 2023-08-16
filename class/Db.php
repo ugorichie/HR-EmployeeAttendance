6918044dcb40c02462bb2f46965d3aa22c3ab44f
oguchi ugochukwu <ugoebukaoguchi@gmail.com>
2023-08-16 15:19:34 +0100

    added class folder, and connected to the Db

---
+++ class/Db.php
@@ -0,0 +1,39 @@
+<?php
+
+class Db{
+        
+    private $dbHost = "localhost";
+    private $dbUsername = "root";
+    private $dbPassword = "";
+    private $dbName = "yipattendancetask";
+
+
+
+    //THIS FUNCTION CONNECTS THIS CLASS(PHP) TO THE DATABASE
+    public function connect(){
+        try{
+
+            $dsn = "mysql:host={$this->dbHost};dbname={$this->dbName}";
+            $conn = new PDO($dsn,$this->dbUsername,$this->dbPassword);
+            $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+
+        }catch(PDOEXECEPTION $e){
+
+            echo $e -> getMessage();
+
+        }
+
+    }
+
+
+}
+
+    //INSTANTIALIZATION HAPPENS HERE TO TELL US IF WE ARE HAVE SUUCCESSFULLY CONNECTED TO THE DATABASE.
+         $demo = new Db();
+       print_r($demo -> connect());
+   
+
+
+
+?>
---
+++ class/Staff.php
@@ -0,0 +1,29 @@
+<?php
+require_once("Db.php");
+
+Class Staff extends Db{
+
+
+
+    //THIS FUNCTION IS RESPONSIBLE FOR FATCHING DETAILS OF ALL THRE STAFF IN TEH DATABASE TO BE DISPALYED WHERE NECESARY
+    public function getStaff(){
+        $sql = "SELECT * FROM Staff ";
+        $stmt = $this -> connect() -> prepare($sql);
+        $stmt -> execute([]);
+        $result = $stmt -> fetchAll(PDO::FETCH_ASSOC);
+        
+        return $result;
+
+    }
+
+
+
+}
+
+$check = new Staff();
+$checker = $check -> getStaff();
+
+print_r ($checker);
+
+
+?>
